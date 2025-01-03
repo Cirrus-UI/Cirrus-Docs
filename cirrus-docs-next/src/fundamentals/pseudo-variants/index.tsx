d28e9299a9a62528783fe094936e84bd59028637
Stanley Lim <slim679975@gmail.com>
2025-01-03 02:57:13 -0800

    :construction: feat(pseudo-variants): Start pseudo-variants doc page

--- cirrus-docs-next/src/fundamentals/pseudo-variants/index.tsx
+++ cirrus-docs-next/src/fundamentals/pseudo-variants/index.tsx
@@ -8,9 +8,8 @@ import { Pagination } from '../../../layouts/components/pagination';
 import { DefaultLayout } from '../../../layouts/default';
 import { toc } from './toc';
 import { PAGE_TITLE_PREFIX } from '../../../constants';
+import { InternalLink } from '../../../layouts/components/link';
 import { CodeBlock } from '../../../layouts/components/codeblock';
-import { TITLE_FUNDAMENTALS } from '../../../config/sidebar';
-import { title } from 'process';
 
 export const PseudoVariantsPage: React.FC<any> = (props) => {
     return (
@@ -30,6 +29,31 @@ export const PseudoVariantsPage: React.FC<any> = (props) => {
                     <div className="content">
                         <Headline title="Responsive" link="#responsive" size="4" />
                         <div className="divider"></div>
+                        <p>
+                            The <code>responsive</code> pseudo-variant is used to generate classes with the{' '}
+                            <code>sm</code>, <code>md</code>, <code>lg</code>, and <code>xl</code> prefixes. These will
+                            apply select styles based on the current viewport.
+                        </p>
+                        <p>
+                            You can read more about it in the <InternalLink url={`./viewports`}>Viewport</InternalLink>{' '}
+                            docs.
+                        </p>
+
+                        <CodeBlock
+                            language="htmlbars"
+                            code={`<div class="u-flex u-flex-column md:u-flex-row">
+    ...                
+</div>`}
+                        />
+                        <CodeBlock
+                            language="scss"
+                            code={`.foo {
+    @extend
+        .u-flex,
+        .u-flex-column,
+        .md\\:u-flex-row;
+}`}
+                        />
                     </div>
                 </section>
 
@@ -37,6 +61,48 @@ export const PseudoVariantsPage: React.FC<any> = (props) => {
                     <div className="content">
                         <Headline title="Dark/Light" link="#dark-light" size="4" />
                         <div className="divider"></div>
+                        <p>
+                            The <code>dark</code> and <code>light</code> pseudo-variants generate class variations that
+                            apply for <code>prefers-color-scheme: dark</code> and{' '}
+                            <code>prefers-color-scheme: light</code> respectively.
+                        </p>
+                        <p>
+                            By default, all classes with the <code>light:</code> prefix will apply even with no{' '}
+                            <code>prefers-color-scheme</code> value set.
+                        </p>
+
+                        <h6 className="font-normal">Dark</h6>
+                        <CodeBlock
+                            language="htmlbars"
+                            code={`<div class="text-black dark:text-gray-100">
+...                
+</div>`}
+                        />
+                        <CodeBlock
+                            language="scss"
+                            code={`.foo {
+    @extend
+        .text-black,
+        .dark\\:text-gray-100;
+}`}
+                        />
+                        <div className="space"></div>
+
+                        <h6 className="font-normal">Light</h6>
+                        <CodeBlock
+                            language="htmlbars"
+                            code={`<div class="text-white light:text-black">
+...                
+</div>`}
+                        />
+                        <CodeBlock
+                            language="scss"
+                            code={`.foo {
+    @extend
+        .text-black,
+        .light\\:text-white;
+}`}
+                        />
                     </div>
                 </section>
 
@@ -44,6 +110,26 @@ export const PseudoVariantsPage: React.FC<any> = (props) => {
                     <div className="content">
                         <Headline title="Reduce Motion" link="#reduce-motion" size="4" />
                         <div className="divider"></div>
+                        <p>
+                            The <code>reduce-motion</code> pseudo-variant generates class variations that
+                            apply for <code>prefers-reduced-motion</code> to apply styles that minimize non-essential motions.
+                        </p>
+                        <p>In the example below, we have styles for an animated class that has no duration for users that have reduced motion enabled.</p>
+                        <CodeBlock
+                            language="htmlbars"
+                            code={`<div class="animated ping reduce-motion:duration-0">
+...                
+</div>`}
+                        />
+                        <CodeBlock
+                            language="scss"
+                            code={`.foo {
+    @extend
+        .animated,
+        .ping,
+        .reduce-motion\\:duration-0;
+}`}
+                        />
                     </div>
                 </section>
 
@@ -51,6 +137,26 @@ export const PseudoVariantsPage: React.FC<any> = (props) => {
                     <div className="content">
                         <Headline title="First/Last of Type" link="#first-last-of-type" size="4" />
                         <div className="divider"></div>
+                        <p>
+                            The <code>first-of-type</code> and <code>last-of-type</code> pseudo-variants generate class variations that
+                            apply on the first and last child elements respectively.
+                        </p>
+                        <CodeBlock
+                            language="htmlbars"
+                            code={`<div class="first-of-type:text-red-700 last-of-type:text-green-700">
+    <p>I am red</p>
+    <p>I am the foreground color</p>
+    <p>I am green</p>
+</div>`}
+                        />
+                        <CodeBlock
+                            language="scss"
+                            code={`.foo {
+    @extend
+        .first-of-type\\:text-red-700,
+        .last-of-type\\:text-green-700;
+}`}
+                        />
                     </div>
                 </section>
 
@@ -58,6 +164,25 @@ export const PseudoVariantsPage: React.FC<any> = (props) => {
                     <div className="content">
                         <Headline title="Portrait/Landscape" link="#portrait-landscape" size="4" />
                         <div className="divider"></div>
+                        <p>
+                            The <code>portrait</code> and <code>landscape</code> pseudo-variants generate class variations that
+                            apply on the portrait and landscape device orientations respectively.
+                        </p>
+                        <CodeBlock
+                            language="htmlbars"
+                            code={`<div class="u-flex portrait:u-flex-column landscape:u-flex-row">
+    ...
+</div>`}
+                        />
+                        <CodeBlock
+                            language="scss"
+                            code={`.foo {
+    @extend
+        .u-flex
+        .portrait\\:u-flex-column,
+        .landscape\\:u-flex-row;
+}`}
+                        />
                     </div>
                 </section>
 
@@ -65,6 +190,40 @@ export const PseudoVariantsPage: React.FC<any> = (props) => {
                     <div className="content">
                         <Headline title="Hover/Group Hover" link="#hover-group-hover" size="4" />
                         <div className="divider"></div>
+                        <p>
+                            The <code>hover</code> pseudo-variant generates class variations that
+                            apply when an element is hovered.
+                        </p>
+                        <CodeBlock
+                            language="htmlbars"
+                            code={`<div class="bg-gray-000 hover:bg-gray-200">
+    ...
+</div>`}
+                        />
+                        <CodeBlock
+                            language="scss"
+                            code={`.foo {
+    @extend
+        .bg-gray-000
+        .hover\\:bg-gray-200;
+}`}
+                        />
+                        <div className="space"></div>
+
+                        <p>To apply a style when hovering a parent/encompassing element, we can use the <code>group-hover</code> pseudo-variants. We just need to add the <code>group</code> class to the parent element and the <code>group-hover:</code> styles to the child elements we want to update.</p>
+                        <CodeBlock
+                            language="htmlbars"
+                            code={`<div class="group">
+   <div class="group-hover:u-opacity-90"></div> 
+</div>`}
+                        />
+                        <CodeBlock
+                            language="scss"
+                            code={`.child {
+    @extend
+        .hover\\:u-opacity-90;
+}`}
+                        />
                     </div>
                 </section>
 
@@ -72,6 +231,39 @@ export const PseudoVariantsPage: React.FC<any> = (props) => {
                     <div className="content">
                         <Headline title="Focus/Group Focus" link="#focus-group-focus" size="4" />
                         <div className="divider"></div>
+                        <p>
+                            The <code>focus</code> pseudo-variant generates class variations that
+                            apply when an element is focused.
+                        </p>
+                        <CodeBlock
+                            language="htmlbars"
+                            code={`<input class="focus:bg-blue-100">
+    ...
+</input>`}
+                        />
+                        <CodeBlock
+                            language="scss"
+                            code={`.foo {
+    @extend
+        .focus\\:bg-blue-100;
+}`}
+                        />
+                        <div className="space"></div>
+
+                        <p>To apply a style when focusing a parent/encompassing element, we can use the <code>group-focus</code> pseudo-variants. We just need to add the <code>group</code> class to the parent element and the <code>group-focus:</code> styles to the child elements we want to update.</p>
+                        <CodeBlock
+                            language="htmlbars"
+                            code={`<div class="group">
+   <div class="group-focus:p-2"></div> 
+</div>`}
+                        />
+                        <CodeBlock
+                            language="scss"
+                            code={`.child {
+    @extend
+        .group-focus\\:p-2;
+}`}
+                        />
                     </div>
                 </section>
 
@@ -79,6 +271,25 @@ export const PseudoVariantsPage: React.FC<any> = (props) => {
                     <div className="content">
                         <Headline title="Focus Visible" link="#focus-visible" size="4" />
                         <div className="divider"></div>
+                        
+                        <p>
+                            The <code>focus-visible</code> pseudo-variant generates class variations that
+                            apply on elements when the focus ring is visible.
+                        </p>
+                        <CodeBlock
+                            language="htmlbars"
+                            code={`<select class="focus-visible:u-border-2 border-green-600">
+...                
+</select>`}
+                        />
+                        <CodeBlock
+                            language="scss"
+                            code={`.foo {
+    @extend
+        .border-green-600,
+        .focus-visible\\:border-green-600;
+}`}
+                        />
                     </div>
                 </section>
 
@@ -86,6 +297,25 @@ export const PseudoVariantsPage: React.FC<any> = (props) => {
                     <div className="content">
                         <Headline title="Focus Within" link="#focus-within" size="4" />
                         <div className="divider"></div>
+                        
+                        <p>
+                            The <code>focus-within</code> pseudo-variant generates class variations that
+                            apply on elements when the element or any of its children are focused.
+                        </p>
+                        <CodeBlock
+                            language="htmlbars"
+                            code={`<select class="focus-visible:u-border-2 border-green-600">
+...                
+</select>`}
+                        />
+                        <CodeBlock
+                            language="scss"
+                            code={`.foo {
+    @extend
+        .border-green-600,
+        .focus-visible\\:border-green-600;
+}`}
+                        />
                     </div>
                 </section>
 
